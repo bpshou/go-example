3c4b4ba593e7df45477548f89b0e578345018a4d
bpshou <decezz@qq.com>
2025-09-02 17:37:41 +0800

    lumberjack

---
+++ lumberjack/go.mod
@@ -0,0 +1,8 @@
+module lumberjack_app
+
+go 1.22.6
+
+require (
+	github.com/google/uuid v1.6.0
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1
+)
---
+++ lumberjack/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log/slog"
+
+	"github.com/google/uuid"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+func main() {
+	InitLogger()
+	slog.Info("hello world", "name", "John", "age", 30) // 这行日志会输出
+
+	LogLevel.Set(slog.LevelError) // 这样就可以在运行时更新日志等级
+
+	slog.Info("hello world", "name", "Rose", "age", 18) // 这行日志不会输出
+
+	slog.Error("error !!!", "error", "error message") // 这行日志会输出
+}
+
+var LogLevel = new(slog.LevelVar)
+
+func InitLogger() {
+	log := lumberjack.Logger{
+		Filename:   "./lumberjack.log", // 日志文件的位置
+		MaxSize:    1,                  // 文件最大尺寸（以MB为单位）
+		MaxAge:     1,                  // 保留旧文件的最大天数
+		MaxBackups: 3,                  // 保留的最大旧文件数量
+		LocalTime:  true,               // 使用本地时间创建时间戳
+		Compress:   true,               // 是否压缩/归档旧文件
+	}
+
+	LogLevel.Set(slog.LevelInfo)
+
+	// 在handler上加上uuid属性
+	handler := slog.NewJSONHandler(&log, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     LogLevel,
+	}).WithAttrs([]slog.Attr{
+		slog.String("uuid", uuid.NewString()),
+	})
+	logger := slog.New(handler)
+	slog.SetDefault(logger)
+}
