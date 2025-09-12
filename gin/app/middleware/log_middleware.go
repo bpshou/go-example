504021c37fd2dfcabed7ab472a8a6ce1cd777c2e
吃柠檬的企鹅 <decezz@qq.com>
2025-09-12 23:11:13 +0800

    gin 使用优化

--- gin/app/middleware/log_middleware.go
+++ gin/app/middleware/log_middleware.go
@@ -9,13 +9,17 @@ import (
 )
 
 func LogMiddleware(c *gin.Context) {
-	log.InitLogger(&lumberjack.Logger{
+	log.InitLogger(GetLumberjackLogger())
+	c.Next()
+}
+
+func GetLumberjackLogger() *lumberjack.Logger {
+	return &lumberjack.Logger{
 		Filename:   viper.GetString("Log.Filename"), // 日志文件的位置
 		MaxSize:    viper.GetInt("Log.MaxSize"),     // 文件最大尺寸（以MB为单位）
 		MaxAge:     viper.GetInt("Log.MaxAge"),      // 保留旧文件的最大天数
 		MaxBackups: viper.GetInt("Log.MaxBackups"),  // 保留的最大旧文件数量
 		LocalTime:  viper.GetBool("Log.LocalTime"),  // 使用本地时间创建时间戳
 		Compress:   viper.GetBool("Log.Compress"),   // 是否压缩/归档旧文件
-	})
-	c.Next()
+	}
 }
--- gin/app/router/run.go
+++ gin/app/router/run.go
@@ -2,14 +2,20 @@ package router
 
 import (
 	"gin_app/app/middleware"
+	"io"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func Run() {
-	engine := gin.Default()
+	engine := gin.New() // gin.Default()
+	// debug
+	gin.SetMode(gin.DebugMode)
+	// 控制台和日志文件双输出
+	gin.DefaultWriter = io.MultiWriter(os.Stdout, middleware.GetLumberjackLogger())
 	// 全局中间件注册
-	engine.Use(middleware.LogMiddleware)
+	engine.Use(gin.Recovery(), gin.Logger(), middleware.LogMiddleware)
 	// 注册路由
 	RegisterRouters(engine)
 
